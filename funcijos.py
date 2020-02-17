c9d1fde3a8c4edfdf241ec54ec0cf255f0fafadd
Donatas Noreika <donoras@gmail.com>
2020-02-17 11:53:27 +0200

    Initial commit

---
+++ funcijos.py
@@ -0,0 +1,32 @@
+
+import sqlalchemy
+
+class Irasas:
+    def __init__(self, suma):
+        self.suma = suma
+
+class PajamuIrasas(Irasas):
+    pass
+
+class IslaiduIrasas(Irasas):
+    pass
+
+
+zurnalas = []
+
+pajamos1 = PajamuIrasas(500)
+zurnalas.append(pajamos1)
+
+islaidos1 = IslaiduIrasas(10)
+zurnalas.append(islaidos1)
+
+islaidos2 = IslaiduIrasas(100)
+zurnalas.append(islaidos2)
+
+suma = 0
+for irasas in zurnalas:
+    if isinstance(irasas, PajamuIrasas):
+        suma += irasas.suma
+    elif isinstance(irasas, IslaiduIrasas):
+        suma -= irasas.suma
+print(suma)
